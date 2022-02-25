5b2174e025773106444d4f4f9330b21b87ffbf80
ryanhagen <rhinory2001@gmail.com>
2022-02-25 15:54:33 -0600

    New names

--- src/main/java/com/interview/intern/message/Application.java
+++ src/main/java/com/interview/intern/message/Application.java
@@ -6,14 +6,14 @@ public class Application {
 	public static void main(String[] args) {
 		MessageHandler handler = new LoggingHandler();
 		MessageRouter.Configuration configuration = MessageRouter.Configuration.builder()
-				.subscribe(new Subscriber("contains=Putin", handler))
-				.subscribe(new Subscriber("contains=Trump", handler))
-				.subscribe(new Subscriber("contains=Biden", handler))
+				.subscribe(new Subscriber("contains=Jerry", handler))
+				.subscribe(new Subscriber("contains=Joey", handler))
+				.subscribe(new Subscriber("contains=Jessie", handler))
 				.subscribe(new Subscriber("contains=Ryan", new HttpMessageHandler()))
 				.build();
 
 		MessageRouter router = new MessageRouter(configuration);
-		router.route(new Message<>("hello there Putin from Ryan!"));
+		router.route(new Message<>("hello there Jerry from Ryan!"));
 	}
 
 }
